e03b46f26bdb463c0ed4eec8d087dd5c3a3ff6f4
maverickPrakash <Prakashtheryon@gmail.com>
2023-09-21 02:19:17 -0400

    Product-service

---
+++ microservices-parents/src/main/java/ca/gbc/microservicesparents/MicroservicesParentsApplication.java
@@ -0,0 +1,13 @@
+package ca.gbc.microservicesparents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesParentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesParentsApplication.class, args);
+	}
+
+}
---
+++ microservices-parents/src/test/java/ca/gbc/microservicesparents/MicroservicesParentsApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gbc.microservicesparents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesParentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
