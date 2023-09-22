3c83ad990dd7cf2466c2af503c35b1408398421a
maverickPrakash <Prakashtheryon@gmail.com>
2023-09-22 01:15:43 -0400

    In class Exercise 1

--- microservices-parents/product-service/src/main/java/ca/gbc/productservice/repository/ProductRepository.java
+++ microservices-parents/product-service/src/main/java/ca/gbc/productservice/repository/ProductRepository.java
@@ -1,2 +1,10 @@
-package ca.gbc.productservice.repository;public interface ProductRepository {
+package ca.gbc.productservice.repository;
+
+import org.springframework.data.mongodb.repository.DeleteQuery;
+import ca.gbc.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product,String> {
+    @DeleteQuery
+    void deleteById(String productId);
 }
